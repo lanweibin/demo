12a56e020c10b432803c37da380ddbe9e8e637ef
lanweibin <lanweibin0203@outlook.com>
2017-11-19 10:32:51 +0800

    数组右循环位移

---
+++ src/main/java/beauty/RightShift.java
@@ -0,0 +1,27 @@
+package beauty;
+
+public class RightShift {
+    public  static int[] Right(int[] arr, int n){
+        while(n-->0){
+            int first = arr[arr.length-1];
+            for (int i =arr.length-1 ; i >0; i--) {
+                arr[i] = arr[i-1];
+
+            }
+            arr[0] = first;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int n = 2;
+        int[] result = Right(arr,2);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] +",");
+
+        }
+    }
+}
